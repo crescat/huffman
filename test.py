8e3764de4918ba3401b9bd91e33f0ce9595e4ff0
cres <crescat@users.noreply.github.com>
2019-06-02 01:10:55 +0800

    write code into command line tool

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from huffman_code import *
 import struct
 import pickle
 import array
+import sys
 
 def bits_to_bytes(bits):
     length = len(bits)
@@ -45,7 +46,7 @@ def decompress(byte_stream):
     return result
 
 
-if __name__  == '__main__':
+def test_with_shakespeare():
     # trying compress
     f1 = open('shakespeare.txt', 'rb')
     f2 = open('shakespeare_compressed', 'wb')
@@ -63,3 +64,27 @@ if __name__  == '__main__':
     f2.write(decompressed_data)
     f1.close()
     f2.close()
+
+def run_with_sys_argv(args):
+    if args[1] not in ['compress', 'decompress']:
+        raise Exception('something wrong!')
+    f1 = open(args[2], 'rb')
+    f2 = open(args[3], 'wb')
+    data = f1.read()
+    if args[1] == 'compress':
+        processed_data = compress(data)
+    elif args[1] == 'decompress':
+        processed_data = decompress(data)
+    f2.write(processed_data)
+    f1.close()
+    f2.close()
+
+
+if __name__  == '__main__':
+    if len(sys.argv) == 4:
+        run_with_sys_argv(sys.arg)
+    else:
+        print(f"""
+        Usage: {sys.argv[0]} ACTION <INPUT FILE> <OUTPUT FILE>
+        ACTION: compress, decompress
+        """)
